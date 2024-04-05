42a715a5fb9a2c6d636076af2bc396264078c2d7
João Barbosa <93934411+JPB02@users.noreply.github.com>
2024-04-05 16:58:15 +0100

    guião 2 ex5_6

--- Guião 4/esqueleto-ex3/matrix.c
+++ Guião 4/esqueleto-ex3/matrix.c
@@ -9,30 +9,7 @@ int **createMatrix() {
     // Allocate and populate matrix with random numbers.
     printf("Generating numbers from 0 to %d...", MAX_RAND);
     int **matrix = (int **) malloc(sizeof(int*) * ROWS);
-    for (int i = 0; i < ROWS; i++) {
-        matrix[i] = (int*) malloc(sizeof(int) * COLUMNS);
-        for (int j = 0; j < COLUMNS; j++) {
-            matrix[i][j] = rand() % MAX_RAND;
-        }
-    }
-    printf("Done.\n");
-
-    return matrix;
-}
-
-void printMatrix(int **matrix) {
-
-    for (int i = 0; i < ROWS; i++) {
-        printf("%2d | ", i);
-        for (int j = 0; j < COLUMNS; j++) {
-            printf("%7d ", matrix[i][j]);
-        }
-        printf("\n");
-    }
-}
-
-// ex.5
-int valueExists(int **matrix, int value) {
+    fint valueExists(int **matrix, int value) {
 
     int pipefd[2];
     int ret = pipe(pipefd);
@@ -91,10 +68,82 @@ int valueExists(int **matrix, int value) {
     }
 
 }
+or (int i = 0; i < ROWS; i++) {
+        matrix[i] = (int*) malloc(sizeof(int) * COLUMNS);
+        for (int j = 0; j < COLUMNS; j++) {
+            matrix[i][j] = rand() % MAX_RAND;
+        }
+    }
+    printf("Done.\n");
+
+    return matrix;
+}
+
+void printMatrix(int **matrix) {
+
+    for (int i = 0; i < ROWS; i++) {
+        printf("%2d | ", i);
+        for (int j = 0; j < COLUMNS; j++) {
+            printf("%7d ", matrix[i][j]);
+        }
+        printf("\n");
+    }
+}
+
+void lookupNumber(int** matrix, int value, int* vector){
+
+    int pfd[2];
+    pipe(pfd);
 
 
-// ex.6
-void linesWithValue(int **matrix, int value) {
 
-    // TO DOm
+    for(int i=0; i<ROWS; i++) {
+        pid_t pid = fork();
+
+        if(pid==0) {
+            //filho
+            Minfo m;
+            m.line_nr = i;
+            m.ocur_nr = 0;
+
+            close(pfd[0]);
+
+            for(int j=0; j<COLUMNS; j++) {
+                if(matrix[i][j] == value) {
+                    m.ocur_nr++;
+                }
+            }
+
+            write(pfd[1], &m, sizeof(m));
+
+            close(pfd[1]);
+            _exit(0);
+        }
+
+    }
+
+    write(pfd[1], &m, sizeof(m));
+
+    close(pfd[1]);
+
+    Minfo res;
+    while(read(pfd[0], &res, sizeof(res)) > 0) {
+        vector[res.line_nr] = res.ocur_nr;
+    }
+
+    for(int i=0; i<ROWS; i++) {
+        int status;
+        wait(&status);
+        res = WEXITSTATUS(status);
+        if(WIFEXITED(status)) {
+            printf("filho terminou\n");
+        }
+
+        else {
+            perror("filho interrompido");
+        }
+
+        close(pfd[0]);
+    }
+
 }
